d79f18a470a092f8931160ba18644d08ee5379ee
enz <enzo_naguiat@yahoo.com>
2019-05-02 10:24:38 +0900

    implement search bar with giphy

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
+// import giphy-api from 'giphy-api';
 
 import Gif from './gif';
 import GifList from './gif_list';
 import SearchBar from './search_bar';
 
+const GIPHY_API_KEY = 'Dzke4GhKWMohfCD7uv0Sni0nvhbRn500';
+const giphy = require('giphy-api')(GIPHY_API_KEY);
 
 class App extends Component {
-  render(){
-    const gifs = [
-      "13M4Ki3u5orBe0",
-      "iU7QUbHXVsS2s",
-      "6WdfqTu3GkQ6s"
-    ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [
+        "13M4Ki3u5orBe0",
+        "iU7QUbHXVsS2s",
+        "6WdfqTu3GkQ6s"
+      ]
+    }
+  }
 
+  giphySearch = (term) => {
+    giphy.search({
+      q: term,
+      rating: 'g',
+      limit: 3
+    }, (error, result) => {
+      let gifs = result.data.map((gif) => {return gif.id});
+      this.setState({gifs: gifs});
+      console.log(this.state.gifs);
+    })
+  }
+
+  render(){
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar giphySearch={this.giphySearch} />
           <Gif id="FvWqHJarQ07x6"/>
         </div>
         <div className="right-scene">
-          <div><GifList gifs={gifs} /></div>
+          <div><GifList gifs={this.state.gifs} /></div>
         </div>
       </div>
     )
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  search = (searchTerm) => {
+    this.setState({searchTerm});
+    console.log(this.props.giphySearch(this.state.searchTerm));;
   }
 
   render() {
     return (
-      <input type="text"  />
+      <input type="text"
+       value={this.state.searchTerm}
+       onChange={event => this.search(event.target.value)}
+      />
     )
   }
 }
